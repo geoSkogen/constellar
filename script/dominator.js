4a6fe5e3c5ded285b87db9e37a9bcd6bf2dc3154
Joseph Scoggins <borninthecaul@gmail.com>
2021-05-20 11:50:33 -0700

    fixes grid layout

--- script/dominator.js
+++ script/dominator.js
@@ -4,15 +4,17 @@ var canvas = document.getElementById('gameboard');
 var grid = document.getElementById('matrix');
 
 
-function assemble_row(row_index, col_count, arg) {
+function assemble_row(row_index, col_count, row_count, arg) {
   var wrapper = document.createElement('section')
   var pre_str = arg ? '' : 'grid'
   var name_str = arg ? 'cell' : 'gridspace'
+  var bound_str = ( !arg && row_index===row_count-1 ) ? ' gridbound_bottom' : ''
   var data_str = arg ? 'gray,gainsboro' : 'brickred,cornflowerblue'
   for (var i = 0; i < col_count; i++){
     var cell = document.createElement('div')
+    bound_str += (i===col_count-1) ? ' gridbound_right' : ''
     cell.id = pre_str + '_' + row_index.toString() + '_' + i.toString()
-    cell.className = name_str
+    cell.className = name_str + bound_str
     cell.setAttribute('data-toggle',data_str)
     wrapper.appendChild(cell)
   }
@@ -23,7 +25,7 @@ function assemble_row(row_index, col_count, arg) {
 
 function assemble_table(wrapper,row_count,col_count,bool) {
   for (var i = 0; i < row_count; i++) {
-    var row = assemble_row(i,col_count,bool)
+    var row = assemble_row(i,col_count,row_count,bool)
     wrapper.appendChild(row)
   }
   return wrapper
@@ -54,9 +56,9 @@ function show_cell_path(row,col,dir) {
   )
 
   if (axis) {
-    el.style.borderLeft = '2px solid black'
+    el.style.borderLeft = '4px solid black'
   } else {
-    el.style.borderTop = '2px solid black'
+    el.style.borderTop = '4px solid black'
   }
 }
 
